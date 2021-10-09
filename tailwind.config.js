d50167bae1f1cedd1f040125020ab943edeaa7f5
Karuna Sharma <79369446+Karunasharma09@users.noreply.github.com>
2021-10-09 20:36:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        c1: '#66FCF1',
+        c2: '#45A29E',
+        c3: '#C5C6C7',
+        c4: '#1F2833',
+        c5: '#0B0C10',
+      },
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
