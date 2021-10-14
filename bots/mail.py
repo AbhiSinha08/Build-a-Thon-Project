aca3bc942cfc15f6688bf13ef11767b135f80458
Karuna Sharma <79369446+Karunasharma09@users.noreply.github.com>
2021-10-14 18:13:35 +0530

    Add files via upload

--- bots/mail.py
+++ bots/mail.py
@@ -1,4 +1,20 @@
 from cfg import MAIL_ID, MAIL_PASSWORD
+import smtplib
 
 def sendMail(reciever, sub, content):
-    pass
+    #header
+    message = f"""From: eXlygenze {MAIL_ID}
+    To: {reciever}
+    Subject: {sub}\n
+    {content} """
+
+    server = smtplib.SMTP("smtp.gmail.com" , 587)
+    server.starttls()
+    try:    
+        server.login(MAIL_ID,MAIL_PASSWORD)
+        print("Logged in...")
+        server.sendmail(MAIL_ID,reciever,message)
+        print("email has been send!")
+    except smtplib.SMTPAuthenticationError:
+        print("Unable to sign in!")
+    
