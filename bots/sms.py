021ad51c3ef4363055763af2f71aa5a5b9d56a07
Abhinav Sinha <64038655+AbhiSinha08@users.noreply.github.com>
2021-10-17 02:59:13 +0530

    Delete sms.py

--- bots/sms.py
+++
@@ -1,8 +0,0 @@
-import pywhatkit
-
-def sendMsg(reciever, content):
-    reciever = f"+91{reciever}"
-    pywhatkit.sendwhatmsg_instantly(reciever, content, wait_time=20, tab_close=True, close_time=3)
-
-if __name__ == '__main__':
-    sendMsg("9016068450", "hmmm")
