7d6227bfc640d6a12565f6389a2f9b42965fe8ba
Karuna Sharma <79369446+Karunasharma09@users.noreply.github.com>
2021-10-17 00:31:07 +0530

    Add files via upload

---
+++ static/admin.js
@@ -0,0 +1,89 @@
+butn1.addEventListener("click", () => {
+  butn1.style.backgroundColor = "#3B82F6"
+  butn1.style.color = 'white'
+  butn2.style.backgroundColor = "white"
+  butn2.style.color = '#1E3A8A'
+  butn3.style.backgroundColor = "white"
+  butn3.style.color = '#1E3A8A'
+  butn4.style.backgroundColor = "white"
+  butn4.style.color = '#1E3A8A'
+  butn5.style.backgroundColor = "white"
+  butn5.style.color = '#1E3A8A'
+  form1.style.display = "block";
+  form2.style.display = "none";
+  form3.style.display = "none";
+  form4.style.display = "none";
+  form5.style.display = "none";
+
+});
+butn2.addEventListener("click", () => {
+  butn2.style.backgroundColor = "#3B82F6"
+  butn2.style.color = 'white'
+  butn1.style.backgroundColor = "white"
+  butn1.style.color = '#1E3A8A'
+  butn3.style.backgroundColor = "white"
+  butn3.style.color = '#1E3A8A'
+  butn4.style.backgroundColor = "white"
+  butn4.style.color = '#1E3A8A'
+  butn5.style.backgroundColor = "white"
+  butn5.style.color = '#1E3A8A'
+  form1.style.display = "none";
+  form2.style.display = "block";
+  form3.style.display = "none";
+  form4.style.display = "none";
+  form5.style.display = "none";
+
+});
+butn3.addEventListener("click", () => {
+  butn1.style.backgroundColor = "white"
+  butn1.style.color = '#1E3A8A'
+  butn2.style.backgroundColor = "white"
+  butn2.style.color = '#1E3A8A'
+  butn3.style.backgroundColor = "#3B82F6"
+  butn3.style.color = 'white'
+  butn4.style.backgroundColor = "white"
+  butn4.style.color = '#1E3A8A'
+  butn5.style.backgroundColor = "white"
+  butn5.style.color = '#1E3A8A'
+  form1.style.display = "none";
+  form2.style.display = "none";
+  form3.style.display = "block";
+  form4.style.display = "none";
+  form5.style.display = "none";
+
+});
+butn4.addEventListener("click", () => {
+  butn1.style.backgroundColor = "white"
+  butn1.style.color = '#1E3A8A'
+  butn3.style.backgroundColor = "white"
+  butn3.style.color = '#1E3A8A'
+  butn2.style.backgroundColor = "white"
+  butn2.style.color = '#1E3A8A'
+  butn4.style.backgroundColor = "#3B82F6"
+  butn4.style.color = 'white'
+  butn5.style.backgroundColor = "white"
+  butn5.style.color = '#1E3A8A'
+  form1.style.display = "none";
+  form2.style.display = "none";
+  form3.style.display = "none";
+  form4.style.display = "block";
+  form5.style.display = "none";
+});
+butn5.addEventListener("click", () => {
+  butn1.style.backgroundColor = "white"
+  butn1.style.color = '#1E3A8A'
+  butn2.style.backgroundColor = "white"
+  butn2.style.color = '#1E3A8A'
+  butn3.style.backgroundColor = "white"
+  butn3.style.color = '#1E3A8A'
+  butn4.style.backgroundColor = "white"
+  butn4.style.color = '#1E3A8A'
+  butn5.style.backgroundColor = "#3B82F6"
+  butn5.style.color = 'white'
+  form1.style.display = "none";
+  form2.style.display = "none";
+  form3.style.display = "none";
+  form4.style.display = "none";
+  form5.style.display = "block";
+
+});
