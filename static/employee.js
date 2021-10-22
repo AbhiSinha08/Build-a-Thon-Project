148fb22845d1836d7e35651e5fcbfe9faeea0503
Abhinav Sinha <64038655+AbhiSinha08@users.noreply.github.com>
2021-10-23 01:43:56 +0530

    Add files via upload

--- static/employee.js
+++ static/employee.js
@@ -18,7 +18,7 @@ function show(data) {
                     clip-rule="evenodd" />
                 <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z" />
             </svg>
-            <span class="pl-3">
+            <span class="pl-3 w-full">
                 <h4 class=" text-green-600 font-bold">`
 
         // Notification Content
@@ -43,7 +43,7 @@ function show(data) {
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
                     clip-rule="evenodd" />
             </svg>
-            <span class="pl-3">
+            <span class="pl-3 w-full">
                 <h4 class="text-yellow-400 font-bold">`
 
         // Notification Content
@@ -68,7 +68,7 @@ function show(data) {
                   d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
                   clip-rule="evenodd" />
           </svg>
-          <span class="pl-3">
+          <span class="pl-3 w-full">
               <h4 class="text-red-500 font-bold">`
 
         // Notification Content
@@ -100,7 +100,20 @@ async function getapi(url) {
     show(data);  // Updating notification on page
 
     // Sending Web Push notification to employee's device
-    var img = '../static/notification.png'; // Icon for notification
+
+    // Icon for notification
+    if (data.type == "rwd") {
+      var img = '../static/reward.png';
+    }
+
+    else if (data.type == "rmd") {
+      var img = '../static/reminder.png';
+    }
+    
+    else if (data.type == "alr") {
+      var img = '../static/alert.png';
+    }
+    
     var head = 'Notification' // Notification Heading
     var text = data.content; // Notification Content
 
